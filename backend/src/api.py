eea5437106008645e7510fc363c3ae9d78e56e0c
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-11 00:43:22 +0200

    meet pep8 code style

--- backend/src/api.py
+++ backend/src/api.py
@@ -26,9 +26,11 @@ implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks}
+     where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
 
 @app.route('/drinks')
 def get_drinks():
@@ -39,15 +41,17 @@ def get_drinks():
         {
             "success": True,
             "drinks": drinks
-            }
-        )
+        }
+    )
+
 
 '''
 implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks}
+     where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 
 '''
@@ -65,15 +69,19 @@ def drinks_detail(payload):
             "drinks": drinks
         })
 
+
 '''
 implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+    returns status code 200 and json {"success": True, "drinks": drink}
+    where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def add_drink(payload):
@@ -81,22 +89,19 @@ def add_drink(payload):
 
     if 'title' and 'recipe' not in body:
         abort(422)
-    
+
     title = body['title']
     recipe = json.dumps(body['recipe'])
     new_drink = Drink(title=title, recipe=recipe)
-    try:
-        new_drink.insert()
-    except:
-        print("can't add new drink")
-        abort(500)
+    new_drink.insert()
+
     return jsonify(
         {
             "success": True,
             "drink": [new_drink.long()]
         }
     )
-      
+
 
 '''
 implement endpoint
@@ -106,9 +111,11 @@ implement endpoint
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+    returns status code 200 and json {"success": True, "drinks": drink}
+    where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+
 
 @app.route('/drinks/<int:id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
@@ -121,9 +128,9 @@ def update_drink(payload, id):
         if 'title' in body:
             drink.title = body['title']
         if 'recipe' in body:
-            drink.recipe = body['recipe'] 
+            drink.recipe = body['recipe']
         drink.update()
-    except:
+    except BaseException:
         print("could not update drink")
         abort(500)
     return jsonify(
@@ -133,7 +140,6 @@ def update_drink(payload, id):
         }
     )
 
-    
 
 '''
 implement endpoint
@@ -142,17 +148,17 @@ implement endpoint
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+    returns status code 200 and json {"success": True, "delete": id}
+    where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+
 @app.route('/drinks/<int:id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drink(payload, id):
     drink = Drink.query.get_or_404(id)
-    try:
-        drink.delete()
-    except:
-        abort(500)
+    drink.delete()
 
     return jsonify(
         {
@@ -160,7 +166,7 @@ def delete_drink(payload, id):
             "delete": id
         }
     )
-    
+
 
 # Error Handling
 
@@ -174,12 +180,12 @@ def unprocessable(error):
     }), 422
 
 
-
-
 '''
 error handler for 404
     error handler should conform to general task above
 '''
+
+
 @app.errorhandler(404)
 def resource_not_found(error):
     return jsonify({
@@ -193,6 +199,8 @@ def resource_not_found(error):
 implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+
 @app.errorhandler(401)
 def unauthorized(error):
     return jsonify({
@@ -201,11 +209,10 @@ def unauthorized(error):
         "message": "unauthorized"
     }), 401
 
+
 @app.errorhandler(AuthError)
 def process_AuthError(error):
     response = jsonify(error.error)
     response.status_code = error.status_code
 
     return response
-
-
--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -10,20 +10,20 @@ AUTH0_DOMAIN = 'wube-fsnd.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffeeshopapi'
 
-## AuthError Exception
+# AuthError Exception
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
-        
 
 
-## Auth Header
-
+# Auth Header
 '''
 @TODO implement get_token_auth_header() method
     it should attempt to get the header from the request
@@ -32,6 +32,8 @@ class AuthError(Exception):
         it should raise an AuthError if the header is malformed
     return the token part of the header
 '''
+
+
 def get_token_auth_header():
     """Obtains the Access Token from the Authorization Header
     """
@@ -64,6 +66,7 @@ def get_token_auth_header():
     token = parts[1]
     return token
 
+
 '''
 implement check_permissions(permission, payload) method
     @INPUTS
@@ -72,15 +75,19 @@ implement check_permissions(permission, payload) method
 
     it should raise an AuthError if permissions are not included in the payload
         !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
+    it should raise an AuthError if the requested permission string is not in
+    the payload permissions array
     return true otherwise
 '''
+
+
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
         abort(400)
     if permission not in payload['permissions']:
         abort(403)
     return True
+
 
 '''
 implement verify_decode_jwt(token) method
@@ -93,23 +100,21 @@ implement verify_decode_jwt(token) method
     it should validate the claims
     return the decoded payload
 
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
+
+
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
-    print('I reached here')
     if 'kid' not in unverified_header:
-        print('inside error message1')
         raise AuthError({
             'code': 'invalid_header',
             'description': 'Authorization malformed.'
         }, 401)
 
     for key in jwks['keys']:
-        # print(key)
         if key['kid'] == unverified_header['kid']:
             rsa_key = {
                 'kty': key['kty'],
@@ -118,11 +123,11 @@ def verify_decode_jwt(token):
                 'n': key['n'],
                 'e': key['e']
             }
-    
+
     if rsa_key:
-        
+
         try:
-           
+
             payload = jwt.decode(
                 token,
                 rsa_key,
@@ -130,7 +135,6 @@ def verify_decode_jwt(token):
                 audience=API_AUDIENCE,
                 issuer='https://' + AUTH0_DOMAIN + '/'
             )
-            print('===============this is payload============')
             return payload
 
         except jwt.ExpiredSignatureError:
@@ -150,9 +154,10 @@ def verify_decode_jwt(token):
                 'description': 'Unable to parse authentication token.'
             }, 400)
     raise AuthError({
-                'code': 'invalid_header',
+        'code': 'invalid_header',
                 'description': 'Unable to find the appropriate key.'
-            }, 400)
+    }, 400)
+
 
 '''
 implement @requires_auth(permission) decorator method
@@ -161,24 +166,24 @@ implement @requires_auth(permission) decorator method
 
     it should use the get_token_auth_header method to get the token
     it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
+    it should use the check_permissions method validate claims and check
+    the requested permission
+    return the decorator which passes the decoded payload
+    to the decorated method
 '''
+
+
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
             token = get_token_auth_header()
             try:
-                # print(token)
                 payload = verify_decode_jwt(token)
-                
             except:
-                print("here is the error")
                 print(sys.exc_info())
                 abort(401)
-                
-                
+
             check_permissions(permission, payload)
             return f(payload, *args, **kwargs)
         return wrapper
