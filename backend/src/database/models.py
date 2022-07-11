bcbff087b70d99856b28c9a717cface23d88714b
wubeshetA <wubeshet.a.y@gmail.com>
2022-07-11 16:14:40 +0200

    add secret variables

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -2,10 +2,12 @@ import os
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
+# from settings import DB_NAME
 
-database_filename = "database.db"
+database_filename = 'database.db'
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = "sqlite:///{}".format(
+    os.path.join(project_dir, database_filename))
 
 db = SQLAlchemy()
 
@@ -26,7 +28,8 @@ def setup_db(app):
 db_drop_and_create_all()
     drops the database tables and starts fresh
     can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+    !!NOTE you can change the database_filename variable to have multiple
+    verisons of a database
 '''
 
 
@@ -39,9 +42,9 @@ def db_drop_and_create_all():
         recipe='[{"name": "water", "color": "blue", "parts": 1}]'
     )
 
-
     drink.insert()
 # ROUTES
+
 
 '''
 Drink
@@ -55,7 +58,8 @@ class Drink(db.Model):
     # String Title
     title = Column(String(80), unique=True)
     # the ingredients blob - this stores a lazy json blob
-    # the required datatype is [{'color': string, 'name':string, 'parts':number}]
+    # the required datatype is [{'color': string, 'name':string,
+    # 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
     '''
@@ -65,7 +69,8 @@ class Drink(db.Model):
 
     def short(self):
         # print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        short_recipe = [{'color': r['color'], 'parts': r['parts']}
+                        for r in json.loads(self.recipe)]
         return {
             'id': self.id,
             'title': self.title,
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,18 @@
 /* @TODO replace with your variables
  * ensure all variables on this page match your project
  */
+const secret = require('secrets.js');
+
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: secret.API_SERVER_URL, // the running FLASK api server url
   auth0: {
-    url: 'wube-fsnd.us', // the auth0 domain prefix
-    audience: 'coffeeshopapi', // the audience set for the auth0 app
-    clientId: 'TjA2fS9x65oui5ZM8rb5h5hH68w1fO8V', // the client id generated for the auth0 app
-    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
+    url: secret.DOMAIN_PREFIX, // the auth0 domain prefix
+    audience: secret.AUDIENCE, // the audience set for the auth0 app
+    clientId: secret.CLIENT_ID, // the client id generated for the auth0 app
+    callbackURL: secret.CALLBACK_URL, // the base url of the running ionic application. 
   }
 };
